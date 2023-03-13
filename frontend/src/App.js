8b26f4975a9bda469967d2de3b0417154fcc3af9
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-03-13 15:58:41 +0630

    frontend: initial sample data binding from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="blue">Button</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
     </div>
   );
 }
